9e0ad30b4da2591b88f388c0acca312037f024b6
shreya purwar <shreyapurwar.03@gmail.com>
2023-04-22 14:49:14 +0530

    commit message

--- services.php
+++ services.php
@@ -51,26 +51,141 @@ include 'connection.php';
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="style2.css">
-</head>
-<title>
- Home Tutors
-</title>
-<h2 class="text text-white">
-    Mentoring of Girls by Girls 
-    <br>
-    Creating an Empowered Society
-    </h2>
+ 
+  <style>
+nav{
+    width:100%;
+  }
+  nav ul li{
+    display:inline-block;
+    list-style:none;
+    width:160px;
+    text-align:center;
+    font-family:Georgia, 'Times New Roman', Times, serif;
+    border: 1px dashed rgba(255,255,255, 0);
+    color:black;
+    padding:10px 0 10px 0;
+    margin:-1px -5px -1px -1px;
+    cursor:pointer;
+    transition:all .2s;
+    -webkit-transition:all .2s;
+    font-size: larger;
+  }
+  
+  
+ 
+  nav{
+    width:100%;
+  }
+  
+  nav ul li:hover{
+    /*border:1px dashed #fff;*/
+    background:rebeccapurple;
+    font-size: x-large;
+  }
+ 
+  nav ul{
+   
+    position:absolute;
+    width:100%;
+    padding:0;
+    z-index:100;
+  }
+  .main {
+    max-width: 300px;
+    padding: 10px;
+    margin: 15px auto;
+    background-color: #fff;
+  }
+ 
+  .logo
+  {
+    background-color: navy;
+  }
+  
+  .logo h2
+  {
+    display: inline;
+    color: pink;
+    text-align: left;
+    animation-name: Shreya;
+    animation-duration: 4s;
+    animation-iteration-count: 1;
+    animation-timing-function: ease-in-out;
+    position: relative;
+  }
+  @keyframes Shreya
+  {
+    from
+    {
+        left: 0px;
+        color: palevioletred;
+    }
+    to
+    {
+        left:300px
+        
+       
+    }
+  }
+  :root {
+    --gradient: linear-gradient(to left top, #DD2476 10%, #FF512F 90%) !important;
+  }
+  .body
+  {
+    background-color:white;
+  }
+  
 
-<body class="bg bg-white ">
+</style>
+  <nav class="nav nav-tab bg bg-danger">
+       
+        <ul class="nav nav-tabs" >
+          <li class="nav-item">
+             <span class="glyphicon glyphicon-home"><a href="home.html">Home</a></span></li>
+       
+            <li class="nav-item">   About
+                <div class="dropdown">
+                    <button class="dropbtn"></button>
+                    <div class="dropdown-content" style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', 
+                    Geneva, Verdana, sans-serif; font-size: medium;" >
+                       Information
+                    </div>
+                
+                  </div>
+                </li>
+        
+          <li class="nav-item">
+            <span class="glyphicon glyphicon-earphone">Contact</span></li>
+          <li class="nav-item">
+            <span class="glyphicon glyphicon-list-alt"><a href="sign.php">Create New Account</a></span>
+          </li>
+           
+          
+        </ul>
+      </nav>
+      <div class="logo" >
+        
+        <img src="hackathon-website.png" width="200px" height="200px" 
+        style="position: relative;left: 500px; ">
+        <h2>We Care For You</h2>
+        <h2 style="position: relative; left:300px">We Believe In You</h2>
+
+        </div>
+</head>
+
+
+<body style="background-color:white">
+    
 <div class="container">
     <div class="row mt-5">
         <div class="column">
             <div class="card">
                 <div class="card-header">
                     <h2 class="display-6  text-centre">
-                        Fetch Data
+                        Home Tutors
 </h2>
+<h4>Empowering Women Through Women</h4>
 </div>
 <div class="card-body">
     <table class="table table-bordered text-centre">
@@ -129,6 +244,7 @@ include 'connection.php';
 
 </div>
 <form method="post">
+            <div class="text text-centre">
         <input type="submit" name="button1"
                 value="Accounts"/>
          
@@ -140,6 +256,7 @@ include 'connection.php';
 
                 <input type="submit" name="button4"
                 value="All"/>
+        </div>
                         
     </form>
 </body>
